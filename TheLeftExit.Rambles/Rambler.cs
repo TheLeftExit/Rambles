235c88ca1dd0facd535e1633cfabdb018c800b35
TheLeftExit <norton@theleftexit.net>
2023-09-22 23:18:16 +0400

    First split test

---
+++ TheLeftExit.Rambles/Rambler.cs
@@ -0,0 +1,29 @@
+﻿public record RambleInfo(string Path, Ramble Ramble);
+
+public record RambleFileInfo(string Path, string Content);
+
+public interface IRambleRenderer
+{
+    IEnumerable<RambleFileInfo> Render(IEnumerable<RambleInfo> rambles);
+}
+
+public record RambleConfiguration(string FromPath, string ToPath);
+
+public static class Rambler
+{
+    private static readonly RambleFileManager fileManager = new RambleFileManager();
+    private static readonly RambleParser parser = new RambleParser();
+
+    public static void Run(IRambleRenderer renderer, RambleConfiguration configuration)
+    {
+        var markdownFiles = fileManager.ReadAllRambles(configuration.FromPath).ToArray();
+        var parsedFiles = markdownFiles.Select(x => new RambleInfo(x.Path, parser.Parse(x.Content))).ToArray();
+        var renderedFiles = renderer.Render(parsedFiles).ToArray();
+        fileManager.CopyNonRamblesAndWriteRambles(configuration.FromPath, configuration.ToPath, renderedFiles);
+    }
+
+    public static void Run<TRenderer>(RambleConfiguration configuration) where TRenderer : IRambleRenderer, new()
+    {
+        Run(new TRenderer(), configuration);
+    }
+}
